1e3fb8a77867881c2ccdbbd50fd74fd444f0e765
Siqi Liu <flycat1989@gmail.com>
2014-12-08 20:45:21 -0500

    addeadded coordinate conversion

---
+++ coordinateConversion.R
@@ -0,0 +1,33 @@
+library(dplyr)
+
+earthR=6367444.7
+coordCalc=function(lon,lat){
+  r=earthR*(90-lat)/180*pi
+  if(lon>=0){
+    coordx=r*cos((90-abs(lon))/180*pi)
+    coordy=0-r*sin((90-abs(lon))/180*pi)
+  }else{
+    coordx=0-r*cos((90-abs(lon))/180*pi)
+    coordy=0-r*sin((90-abs(lon))/180*pi)
+  }
+  resultCoord=c(2*coordx,2*coordy)
+  return(resultCoord)
+}
+
+load("finalData.rda")
+#testLon=c(-170,-150,-130,-90,-20,10,50,85,135)
+#testLat=c(72,77,78,77,71,85,88,90,75)
+#testData=data.frame(Lon=testLon,Lat=testLat)
+#testData=cbind(testData,Xcoord=NA,Ycoord=NA)
+finalData=cbind(finalData,Xcoord=NA,Ycoord=NA)
+XcoordVec=double()
+YcoordVec=double()
+for(i in 1:4896){
+  result=coordCalc(finalData$longitude[i],finalData$latitude[i])
+  XcoordVec=append(XcoordVec,result[1])
+  YcoordVec=append(YcoordVec,result[2])
+}
+finalData$Xcoord=rep(XcoordVec,420)
+finalData$Ycoord=rep(YcoordVec,420)
+convertedTempData=finalData
+save(convertedTempData,file="convertedTempData.rda")
--- iceshapeModel.R
+++ iceshapeModel.R
@@ -2,10 +2,11 @@ library(rgeos)
 library(raster)
 library(rgdal)
 
-shape=readOGR(paste0("Cache/Shapefiles/","extent_N_197901_polygon"), "extent_N_197901_polygon")
-plot(shape)
-segments(-5000000,-6000000,-5000000,6000000)
-segments(5000000,-6000000,5000000,6000000)
-segments(-5000000,-6000000,5000000,-6000000)
-segments(-5000000,6000000,5000000,6000000)
+shape=readOGR(paste0("Cache/Shapefiles/","extent_N_201309_polygon"), "extent_N_201309_polygon")
+shape1=shape[shape$INDEX==0,]
+plot(shape1)
+segments(-2200000,-2200000,-2200000,2200000)
+segments(1500000,-2500000,1500000,2500000)
+segments(-1500000,-2500000,1500000,-2500000)
+segments(-1500000,2500000,1500000,2500000)
 
